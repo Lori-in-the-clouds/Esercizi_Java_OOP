ffab68034cdf327355d158dabb0b4c3c92a57f98
Lori-in-the-clouds <lorenzodimaio02@gmail.com>
2023-08-23 11:50:54 +0200

    commit mattina 3 23/08/2023

--- src/main/java/threads/ProducerConsumerUnsafe.java
+++ src/main/java/threads/ProducerConsumerUnsafe.java
@@ -1,61 +1,60 @@
 package threads;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 import static java.lang.Thread.sleep;
 
 public class ProducerConsumerUnsafe {
-    public static class consumer extends Thread {
-        Deque<Integer> c = (Deque<Integer>)Collections.synchronizedCollection(new ArrayDeque<Integer>());
+    public static void main(String[] args) throws InterruptedException {
+        Deque<Integer> deq = new ConcurrentLinkedDeque<>();
+        Thread t1 = new producer(deq);
+        Thread t2 = new consumer(deq);
+        t1.start();
+        t2.start();
+        sleep(100L);
+        t1.interrupt();
+        t2.interrupt();
+    }
 
-        public consumer(Deque<Integer> c) {
-            this.c = c;
+    public static class producer extends Thread {
+        final Deque<Integer> deq;
+
+        public producer(Deque<Integer> deq) {
+            super();
+            this.deq = deq;
         }
 
         @Override
         public void run() {
-            int last = 0;
-            while(!interrupted()) {
+            int i =0;
+            while(!isInterrupted()) {
+                deq.add(i++);
+            }
+        }
+    }
+
+    public static class consumer extends Thread {
+        final Deque<Integer> deq;
+
+        public consumer(Deque<Integer> deq) {
+            this.deq = deq;
+        }
+
+        @Override
+        public void run() {
+            int last=0;
+            while(!isInterrupted()) {
                 try {
-                    last = c.removeLast();
-                } catch (NoSuchElementException e) {
+                    last = deq.removeLast();
+
+                } catch (NoSuchElementException no_elem) {
                     Thread.yield();
                 }
             }
             System.out.println("Total number of consumed elements: "+last);
         }
     }
-
-    public static class producer extends Thread {
-        Deque<Integer> c = (Deque<Integer>)Collections.synchronizedCollection(new ArrayDeque<Integer>());
-
-
-        public producer(Deque<Integer> c) {
-            this.c = c;
-        }
-
-        @Override
-        public void run() {
-            int i=0;
-            while(!interrupted()) {
-                c.add(i++);
-            }
-       }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        Deque<Integer> c = new ConcurrentLinkedDeque<>();
-        Thread t1 = new consumer(c);
-        Thread t2 = new producer(c);
-        t1.start();
-        t2.start();
-        Thread.sleep(100L);
-        t1.interrupt();
-        t2.interrupt();
-
-        t1.join();
-        t2.join();
-
-    }
 }
+
