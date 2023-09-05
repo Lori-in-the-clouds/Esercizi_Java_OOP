930d69243f97d2469bb8b13043b33c9c9bd74198
Lori-in-the-clouds <lorenzodimaio02@gmail.com>
2023-09-05 17:10:23 +0200

    commit 2 05/09/2023

--- src/main/java/generics/Reverse.java
+++ src/main/java/generics/Reverse.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Reverse {
     public static <T> void reverse(List<T> list) {
-        for (int i = 0; i<list.size();i++)  {
-            list.add(list.size(),list.get(0));
-            list.remove(0);
+        int size = list.size();
+        for (int i=0;i<Math.floor(size/2);i++) {
+            T tmp = list.get(i);
+            list.set(i,list.get(size-i-1));
+            list.set(size-i-1, tmp);
         }
-
     }
 }
