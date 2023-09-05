10043dc72f656bd9733456201e421480963c3f6d
Lori-in-the-clouds <lorenzodimaio02@gmail.com>
2023-09-05 17:09:01 +0200

    commit 05/09/2023

--- src/main/java/generics/Reverse.java
+++ src/main/java/generics/Reverse.java
@@ -11,14 +11,4 @@ public class Reverse {
         }
 
     }
-
-    public static void main(String[] args) {
-        List<Integer> prova = new ArrayList<>();
-        prova.add(1);
-        prova.add(2);
-        prova.add(3);
-        prova.add(4);
-        prova.add(5);
-        reverse(prova);
-    }
 }
